d965872d5e383e3790a9273ef05fa0ae15e4428c
Juan Carlos Espinoza <macespinozadesign@gmai.com>
2021-12-06 22:53:47 -0600

    Hola Mundo
    creación de primer script Hola Mundo

---
+++ Proyecto Alpha/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("algo paso");
+        Debug.Log("algo paso");
+        Debug.LogWarning("algo salio medianamente mal");
+        Debug.LogError("algo salio muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
